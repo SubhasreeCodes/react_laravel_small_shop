da9a8fb79da481443dd34ac537f7cd99e079047d
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-10-04 14:47:12 +0530

    added basic routes
    Added basic routes Home.jsx, AppLayout.jsx, Footer.jsx, Header.jsx, Topbar.jsx, Login.jsx, Register.jsx and App.jsx

---
+++ src/components/frontend/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      {/* Header content */}
+      Header
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/frontend/layout/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = () => {
+  return (
+    <div>
+      {/* Top bar content */}
+      TopBar
+    </div>
+  );
+};
+
+export default TopBar;
